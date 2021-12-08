d4fd5e57ec80f3f0306dcca1a0579bb3f4a16bd2
Avni Patel <avnip1296@gmail.com>
2021-12-08 18:50:25 +0530

    Vue.js Demo

---
+++ src/components/Modal.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>{{ header }}</h1>
+            <p>Modal component</p>
+        </div>
+    </div>
+</template>
+<script>
+    export default { 
+        props: ['header']
+    }
+</script>
